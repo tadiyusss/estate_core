15656b392d8b3960bffbe32dabe86a1d549b2404
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-08-10 16:24:21 +0800

    turned description field into textareafield

--- forms/property_type.py
+++ forms/property_type.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 class PropertyTypeForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=100)], render_kw={'class': 'fd-input'}, description="Condo, House, Apartment, etc.")
-    description = StringField('Description', validators=[DataRequired(), Length(max=255)], render_kw={'class': 'fd-input'}, description="A brief description of the property type.")
+    description = TextAreaField('Description', validators=[DataRequired(), Length(max=255)], render_kw={'class': 'fd-input', 'rows': 5}, description="A brief description of the property type.")
