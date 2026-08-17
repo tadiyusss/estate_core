d5bbcf82864cf8b143c9a988733a50982a5d303d
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-08-17 19:40:31 +0800

    navbar responsiveness, contact us forms on index, and slight adjustments on public pages

--- routes/public/index.py
+++ routes/public/index.py
@@ -2,9 +2,27 @@ from extensions.estate_core import bp
 from flask import render_template, request, redirect, url_for, flash
 from core.extensions import db
 from flask_login import current_user
-
+from extensions.landing_page.forms.contact_us import ContactUsForm
+from extensions.landing_page.models import TeamMember
+from extensions.landing_page.models import ContactUs
 
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('public/index.html')
+    form = ContactUsForm()
+    team_members = TeamMember.query.all()
+    if form.validate_on_submit():
+        contact_us_entry = ContactUs(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            number_code=form.number_code.data,
+            phone_number=form.phone_number.data,
+            message=form.message.data
+        )
+        db.session.add(contact_us_entry)
+        db.session.commit()
+        
+        return render_template('public/index.html', form=form, success=True, team_members=team_members)
+    
+    return render_template('public/index.html', form=form, team_members=team_members)
 
