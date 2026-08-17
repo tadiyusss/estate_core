ac266052ae5468fd1d9551c7e37aebefc500a2f7
Thaddeus Toledo <toledothaddeusf@gmail.com>
2026-08-17 17:17:07 +0800

    added the responsive design for filter property listing form

---
+++ forms/filters/public_property_listing.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from extensions.estate_core.models.property_listing import STATUS_CHOICES
+
+class FilterPublicPropertyListingForm(FlaskForm):
+    location = SelectField('Location', choices=[("", "All Locations")], render_kw={"class": "w-full px-2 py-2 rounded focus:ring-2 focus:ring-emerald-500/50 focus:outline-none md:text-base text-sm [&>option]:text-zinc-900"})
+    price = SelectField('Price', choices=[("", "All Prices")], render_kw={"class": "w-full px-2 py-2 rounded focus:ring-2 focus:ring-emerald-500/50 focus:outline-none md:text-base text-sm [&>option]:text-zinc-900"})
+    status = SelectField('Status', choices=[("", "All Statuses")] + list(STATUS_CHOICES), render_kw={"class": "w-full px-2 py-2 rounded focus:ring-2 focus:ring-emerald-500/50 focus:outline-none md:text-base text-sm [&>option]:text-zinc-900"})
+    property_type = SelectField('Property Type', choices=[("", "All Types")], render_kw={"class": "w-full px-2 py-2 rounded focus:ring-2 focus:ring-emerald-500/50 focus:outline-none md:text-base text-sm [&>option]:text-zinc-900"})
--- routes/public/property_listing.py
+++ routes/public/property_listing.py
@@ -2,6 +2,8 @@ from extensions.estate_core import bp
 from flask import render_template, request, redirect, url_for, flash
 from core.extensions import db
 from extensions.estate_core.models import PropertyListing
+from extensions.estate_core.forms.filters.public_property_listing import FilterPublicPropertyListingForm
+from extensions.estate_core.models import PropertyType
 
 @bp.app_template_global()
 def format_price(price):
@@ -14,6 +16,31 @@ def format_price(price):
 
 @bp.route('/property-listing', methods=['GET', 'POST'])
 def property_listing():
-    property_listings = PropertyListing.query.all()
-    return render_template('public/property_listing.html', property_listings=property_listings)
+    form = FilterPublicPropertyListingForm()
+    property_listings = PropertyListing.query
+
+    locations = db.session.query(PropertyListing.location).distinct().all()
+    prices = db.session.query(PropertyListing.start_price_range).distinct().all()
+    property_types = PropertyType.query.all()
+
+    form.property_type.choices = [("", "All Types")] + [(ptype.name, ptype.name) for ptype in property_types]
+    form.price.choices = [("", "All Prices")] + [(price[0], f"₱{price[0]:,.2f}") for price in prices]
+    form.location.choices = [("", "All Locations")] + [(loc[0], loc[0]) for loc in locations]
+
+    if request.args.get('location'):
+        property_listings = property_listings.filter_by(location=request.args.get('location'))
+        form.location.data = request.args.get('location')
+    if request.args.get('price'):
+        property_listings = property_listings.filter_by(start_price_range=request.args.get('price'))
+        form.price.data = request.args.get('price')
+    if request.args.get('status'):
+        property_listings = property_listings.filter_by(status=request.args.get('status'))
+        form.status.data = request.args.get('status')
+    if request.args.get('property_type'):
+        property_type = PropertyType.query.filter_by(name=request.args.get('property_type')).first()
+        if property_type:
+            property_listings = property_listings.filter_by(property_type=property_type)
+        form.property_type.data = request.args.get('property_type')
+
+    return render_template('public/property_listing.html', property_listings=property_listings.all(), form=form)
 
